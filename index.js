20778b1880119e44e9bb26d9a92403cc3c52e9d2
abdul momin <mominurrahman10748@gmail.com>
2022-11-08 19:12:38 +0600

    problem soved

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
@@ -22,14 +23,4 @@ const collection = client.db("test").collection("devices");
 app.listen(port, () => {
     console.log(`personal Service Servier Is running port ${port} ${port}`);
 })
-
-
-
-
-
-
-
-
-
-
 
